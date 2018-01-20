9c3417f069d89fcb4ec4900f8010c3a2881048b8
spathak99 <shardoolpathak@gmail.com>
2018-01-20 03:20:27 -0500

    still fixing up ui

--- Backend-flask/appengine_config.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from google.appengine.ext import vendor
-vendor.add('lib')
-on_appengine = os.environ.get('SERVER_SOFTWARE','').startswith('Development')
-if on_appengine and os.name == 'nt':
-    import ctypes
-    os.name = None
